b5d91a9fbf39c694d5b3de06218f6b66f1cf3998
maton369 <ysk901085@gmail.com>
2025-07-05 11:55:53 +0900

    Add: 各ページごとにウィンドウタイトルを設定

---
+++ frontend/src/components/pageTitles.ts
@@ -0,0 +1,7 @@
+// src/constants/pageTitles.ts
+
+export const PAGE_TITLES: Record<string, string> = {
+    "/": "タレントマネジメントシステム - 社員検索",
+    "/employee/[id]": "タレントマネジメントシステム - 社員詳細",
+    // 必要に応じて追加
+};
